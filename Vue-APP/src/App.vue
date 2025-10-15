5d674a144cddb9a416b1a1dafd3b22474b6a4426
smootysmart <dywtsmooty@gmail.com>
2025-10-15 17:33:06 +0700

    [fix] workflow 17:33

--- Vue-APP/src/App.vue
+++ Vue-APP/src/App.vue
@@ -10,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <a>Go APP is running on port 3000</a>
+    <a>Go APP is running on port 300</a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
