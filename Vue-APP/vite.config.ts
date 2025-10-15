b1d7d72aa5f048e7cff04d7641da8cc267dd5cf5
smootysmart <dywtsmooty@gmail.com>
2025-10-15 22:35:05 +0700

    [fix] workflow

--- Vue-APP/vite.config.ts
+++ Vue-APP/vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 80, // Replace 3000 with your desired port
+  },
 })
